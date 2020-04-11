98cbad71860f394460accaaf77c679dd7e9fc2b7
wrongwrong <boranti1995@gmail.com>
2020-04-11 16:17:16 +0900

    KMapperを追加

---
+++ src/main/java/com/javaetmoi/benchmark/mapping/mapper/kmapper/KMaapper.kt
@@ -0,0 +1,31 @@
+package com.javaetmoi.benchmark.mapping.mapper.kmapper
+
+import com.javaetmoi.benchmark.mapping.mapper.OrderMapper
+import com.javaetmoi.benchmark.mapping.model.dto.OrderDTO
+import com.javaetmoi.benchmark.mapping.model.dto.ProductDTO
+import com.javaetmoi.benchmark.mapping.model.entity.Customer
+import com.javaetmoi.benchmark.mapping.model.entity.Order
+import com.javaetmoi.benchmark.mapping.model.entity.Product
+import com.mapk.kmapper.BoundKMapper
+import com.mapk.kmapper.KMapper as Mapper
+
+class KMaapper : OrderMapper {
+    override fun map(source: Order): OrderDTO = orderMapper.map(source)
+
+    companion object {
+        private val productMapper: BoundKMapper<Product, ProductDTO> = BoundKMapper(::ProductDTO, Product::class)
+
+        private fun toDto(customer: Customer, products: List<Product>): OrderDTO {
+            return OrderDTO(
+                    products.map { productMapper.map(it) },
+                    customer.name,
+                    customer.shippingAddress?.street,
+                    customer.shippingAddress?.city,
+                    customer.billingAddress.street,
+                    customer.billingAddress.city
+            )
+        }
+
+        private val orderMapper: Mapper<OrderDTO> = Mapper(::toDto)
+    }
+}
