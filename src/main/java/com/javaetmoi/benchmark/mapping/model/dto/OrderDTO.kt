55f6b8d05160ba8d789656f4b841d61157c3eed3
wrongwrong <boranti1995@gmail.com>
2020-04-11 15:18:37 +0900

    Kotlin化

---
+++ src/main/java/com/javaetmoi/benchmark/mapping/model/dto/OrderDTO.kt
@@ -0,0 +1,13 @@
+package com.javaetmoi.benchmark.mapping.model.dto
+
+import com.javaetmoi.benchmark.mapping.model.Data
+
+@Data
+data class OrderDTO(
+        var products: List<ProductDTO>,
+        var customerName: String,
+        var shippingStreetAddress: String,
+        var shippingCity: String,
+        var billingStreetAddress: String,
+        var billingCity: String
+)
---
+++ src/main/java/com/javaetmoi/benchmark/mapping/model/dto/ProductDTO.kt
@@ -0,0 +1,6 @@
+package com.javaetmoi.benchmark.mapping.model.dto
+
+import com.javaetmoi.benchmark.mapping.model.Data
+
+@Data
+data class ProductDTO(var name: String)
