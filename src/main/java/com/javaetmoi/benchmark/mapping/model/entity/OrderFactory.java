38d24c5845bd5a139a91e0afd3ed8e2764c78c2c
wrongwrong <boranti1995@gmail.com>
2020-04-11 15:31:37 +0900

    Kotlin化対応

--- src/main/java/com/javaetmoi/benchmark/mapping/model/entity/OrderFactory.java
+++ src/main/java/com/javaetmoi/benchmark/mapping/model/entity/OrderFactory.java
@@ -1,40 +1,26 @@
 package com.javaetmoi.benchmark.mapping.model.entity;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 public class OrderFactory {
 
     public static Order buildOrder() {
-        Order order = new Order();
-        Customer customer = new Customer();
-        order.setCustomer(customer);
-        customer.setName("Joe Smith");
-        Address billingAddress = new Address();
-        customer.setBillingAddress(billingAddress);
-        billingAddress.setStreet("1234 Market Street");
-        billingAddress.setCity("San Fran");
-        Address shippingAddress = new Address();
-        customer.setShippingAddress(shippingAddress);
-        shippingAddress.setStreet("1234 West Townsend");
-        shippingAddress.setCity("Boston");
-        List<Product> products = new ArrayList<Product>(2);
-        order.setProducts(products);
-        products.add(new Product("socks"));
-        products.add(new Product("shoes"));
-        return order;
+        Address shippingAddress = new Address("1234 West Townsend", "Boston");
+        Address billingAddress = new Address("1234 Market Street", "San Fran");
+
+        Customer customer = new Customer("Joe Smith", shippingAddress, billingAddress);
+
+        return new Order(
+                customer, Arrays.asList(new Product("socks"), new Product("shoes"))
+        );
     }
 
     public static Order buildPartialOrder() {
-        Order order = new Order();
-        Customer customer = new Customer();
-        order.setCustomer(customer);
-        customer.setName("John Doe");
-        Address billingAddress = new Address();
-        customer.setBillingAddress(billingAddress);
-        billingAddress.setStreet("93 Newcastle Dr.");
-        order.setProducts(Collections.emptyList());
-        return order;
+        Address billingAddress = new Address("93 Newcastle Dr.", null);
+
+        Customer customer = new Customer("John Doe", null, billingAddress);
+
+        return new Order(customer, Collections.emptyList());
     }
 }
